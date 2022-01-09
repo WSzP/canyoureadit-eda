fa6ac2bdfb7505b8f178cbf1d211a4dfa963bd1e
WSzP <peter@kaizen-ux.com>
2022-01-09 03:08:31 +0200

    updated .gitignore

---
+++ constants.py
@@ -0,0 +1,15 @@
+# Path constants
+TIMESTAMP = '202201080305'
+IMAGES = r'data/images_'+TIMESTAMP+'.csv'
+DESKTOP = r'data/response_desktop_'+TIMESTAMP+'.csv'
+MOBILE = r'data/response_mobile_'+TIMESTAMP+'.csv'
+SAVEPATH = r'data/canyoureadit.csv'
+
+# Text formatting constants
+class T:
+   B = '\033[94m' # blue
+   G = '\033[92m' # green
+   Y = '\033[93m' # yellow
+   R = '\033[91m' # red
+   b = '\033[1m' # bold
+   E = '\033[0m' # end formatting
