99925e253d05a7247b59620a727b8c9ec56dc79d
WSzP <peter@kaizen-ux.com>
2022-01-28 10:46:11 +0200

    modification for directory structure change

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ IMAGES = r'data/images_'+TIMESTAMP+'.csv'
 DESKTOP = r'data/response_desktop_'+TIMESTAMP+'.csv'
 MOBILE = r'data/response_mobile_'+TIMESTAMP+'.csv'
 SAVEPATH = r'data/canyoureadit.csv'
-EXPORTDIR = 'data/export'
+EXPORTDIR = r'export/'
 
 # Text formatting constants
 class T:
