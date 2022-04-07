17935b799d282fbbe1793f4c4be29477f74cfd34
WSzP <peter@kaizen-ux.com>
2022-04-07 20:03:44 +0300

    april 7 data update, getting ready for a 1yr milestone

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 # Path constants
-TIMESTAMP = '202201080305'
+TIMESTAMP = '202204071925'
+ENDTIME = '4/7/2022'
 IMAGES = r'data/images_'+TIMESTAMP+'.csv'
 DESKTOP = r'data/response_desktop_'+TIMESTAMP+'.csv'
 MOBILE = r'data/response_mobile_'+TIMESTAMP+'.csv'
